0fa1c916701f22146b32f83097d0d750ade3435c
Dmitriy <timon@yahoo.com>
2022-12-10 11:37:20 +0300

    Задание 36

---
+++ work5_36/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 36. Необходимо подсчитать кол-во четных трехзначных чисел в заданном массиве
+
+Console.Write("Введите размер массива: ");
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+int i = 0;
+int a = 0;
+int sum = 0;
+while (i < arr.Length)
+{
+    arr[i] = new Random().Next(-100, 100);
+    i++;
+}
+Console.WriteLine("Наш массив:");
+for (int k = 0; k < arr.Length; k++)
+{
+    Console.Write(arr[k] + " ");
+}
+Console.WriteLine();
+
+while (a < arr.Length)
+{
+   if(a % 2 == 0)
+    {
+        sum = sum + arr[a];
+        a++;
+    }
+    else 
+    {
+        a++;
+    }
+}
+
+Console.WriteLine("Сумма эелементов, стоящих на нечетных позициях в массиве: " + sum);
