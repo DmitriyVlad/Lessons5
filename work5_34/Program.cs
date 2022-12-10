7a52eb33214b22d3b37a34f5fa8b9e0f78126ca3
Dmitriy <timon@yahoo.com>
2022-12-10 11:16:02 +0300

    Задание 34

---
+++ work5_34/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 34. Необходимо подсчитать кол-во четных трехзначных чисел в заданном массиве
+
+Console.Write("Введите размер массива: ");
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+int i = 0;
+int a = 0;
+int sum = 0;
+while (i < arr.Length)
+{
+    arr[i] = new Random().Next(100, 999);
+    i++;
+}
+Console.WriteLine("Наш массив:");
+for (int k = 0; k < arr.Length; k++)
+{
+    Console.Write(arr[k] + " ");
+}
+Console.WriteLine();
+
+while (a < arr.Length)
+{
+   if(arr[a] % 2 == 0)
+    {
+        sum++;
+        a++;
+    }
+    else 
+    {
+        a++;
+    }
+}
+
+Console.WriteLine("Количество четных чисел в массиве - " + sum);
